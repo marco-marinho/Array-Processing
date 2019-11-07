2a664f3a1cb56e21c89bbf983ddff86e6cfc70a0
Marco Marinho <marcoobom@gmail.com>
2019-11-07 14:02:34 +0100

    Added ESPRIT doa estimator

--- Tester.py
+++ Tester.py
@@ -5,12 +5,15 @@ import DOAEstimators as est
 import numpy as np
 import AlgebraLibrary as alg
 
-A = str.generate_ula_vectors([-45, 60], 8)
+A = str.generate_ula_vectors([-45, 60], 8, 1/2)
 S = sig.gen_signal(2, 100, 15)
 
 X = np.matmul(A, S)
 
-print(est.ESPRIT(X, 2))
+angular_power, angles = est.conventional_beamformer(X, 2, 1/2)
+
+plt.plot(angles, angular_power)
+plt.show()
 
 
 
