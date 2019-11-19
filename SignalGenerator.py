e06cdb831c85b4ef9c3bc875e8aa19cb9c40e69f
Marco Marinho <marcoobom@gmail.com>
2019-11-19 17:37:04 +0100

    Added OFDM signal generation, still some issues to be solved.

--- SignalGenerator.py
+++ SignalGenerator.py
@@ -1,5 +1,7 @@
 import numpy as np
+import scipy.signal as sig
 import math
+import matplotlib.pyplot as plt
 
 #Generates 4 QAM symbols for given number of sources and snapshots with specified SNR
 def gen_signal(sources, snapshots, SNR = np.inf):
@@ -17,3 +19,56 @@ def add_noise(signal, SNR):
     noise = np.sqrt(N0/2) * np.add(np.random.randn(*signal.shape), 1j*np.random.randn(*signal.shape))
     noisy_signal = np.add(signal, noise)
     return noisy_signal
+
+def generate_OFDM_signal(snapshots, subcarriers, channel, cyclic_prefix_lenght, SNR, plot_sent = False, plot_received = False):
+
+    #Calculate number of OFDM data frames
+    frame_number = int(snapshots/subcarriers)
+
+    #Generate random polar signaling data
+    s_data = np.random.choice([1, -1], snapshots) + 1j*np.random.choice([1, -1], snapshots)
+    print(np.shape(s_data))
+    if plot_sent:
+        X = [x.real for x in s_data]
+        Y = [x.imag for x in s_data]
+        plt.scatter(X, Y, color='blue')
+        plt.show()
+
+    #FIR channel in frequency domain
+    hf = np.fft.fft(channel, subcarriers)
+
+    #S/P conversion
+    p_data = np.reshape(s_data, (subcarriers, frame_number))
+
+    #Convert data to time domain
+    p_td = np.fft.ifft(p_data)
+
+    #Get cyclic prefix of given lenght
+    cyclic_prefix = p_td[-cyclic_prefix_lenght:, :]
+
+    #Data frames with cyclic prefix
+    p_cyc = np.concatenate((cyclic_prefix, p_td), axis=0)
+
+    #P/S conversion
+    s_cyc = np.reshape(p_cyc, ((subcarriers+cyclic_prefix_lenght)*frame_number, 1))
+
+    #Pass data through channel
+    chs_out = sig.lfilter(channel, 1, s_cyc, axis=0)
+
+    #Add noise
+    x_out = chs_out#add_noise(chs_out, SNR)
+
+    #P/S conversion and cyclic prefix removal
+    x_para = np.reshape(x_out, (subcarriers+cyclic_prefix_lenght, frame_number))
+    x_disc = x_para[cyclic_prefix_lenght:, :]
+
+    #FFT to F domain
+    x_hat_para = np.fft.fft(x_disc)
+
+    z_data = np.matmul(np.linalg.inv(np.diag(hf)), x_hat_para)
+
+    if plot_received:
+        X = [x.real for x in z_data[10,:]]
+        Y = [x.imag for x in z_data[10,:]]
+        plt.scatter(X, Y, color='red')
+        plt.show()
--- Tester.py
+++ Tester.py
@@ -15,15 +15,15 @@ import AlgebraLibrary as alg
 # plt.plot(angles, angular_power)
 # plt.show()
 
-positions = [[-1, 1], [-1, 0], [-1, -1], [-0.5, 0.5], [-0.5, -0.5], [0, 1], [0, 0], [0, -1], [0.5, 0.5], [0.5, -0.5], [1, 1], [1, 0], [1, -1]]
-A = str.generate_sparse_vectors([60, ], positions, 0.125)
-S = sig.gen_signal(1, 100, 15)
-X = np.matmul(A, S)
-angular_power, angles = est.CAPON_MVDR_sparse(X, 1, positions, 0.125)
+# positions = [[-1, 1], [-1, 0], [-1, -1], [-0.5, 0.5], [-0.5, -0.5], [0, 1], [0, 0], [0, -1], [0.5, 0.5], [0.5, -0.5], [1, 1], [1, 0], [1, -1]]
+# A = str.generate_sparse_vectors([60, ], positions, 0.125)
+# S = sig.gen_signal(1, 100, 15)
+# X = np.matmul(A, S)
+# angular_power, angles = est.CAPON_MVDR_sparse(X, 1, positions, 0.125)
+#
+# plt.plot(angles, angular_power)
+# plt.show()
 
-plt.plot(angles, angular_power)
-plt.show()
 
-
-
+s = sig.generate_OFDM_signal(16000, 32, [0.3, -0.5, 0, 1, 0.2, -0.3], 5, 30, True, True)
 
