f084536340d8086f9b5527dab79c02a3e395011b
Marco Marinho <marcoobom@gmail.com>
2020-02-05 15:03:06 +0100

    Changed simulation geometry, added pairing for distance values to match order of SAGE parameters

--- SimulationKnownDistance.py
+++ SimulationKnownDistance.py
@@ -15,37 +15,50 @@ distances = []
 
 for reflector in reflectors:
     angles.append(geo.getIncidenceAngle(reflector, receiver, True))
-    reflections.append(geo.getReflectionAngle(transmitter, reflector, receiver, True))
+    reflections.append(geo.getReflectionAngle2(transmitter, reflector, receiver, True))
     distances.append(geo.getPointDistance(transmitter, reflector)+geo.getPointDistance(reflector, receiver))
+    line = geo.Line.fromPoints(transmitter, reflector)
+# print(angles)
+# print(reflections)
+total_tries = 1000
 
-A = str.generate_ula_vectors(angles, 15, 1/2)
-S = sig.gen_signal(2, 100)
+SNRs = np.arange(5, 30, 5)
+errors = [0]*len(SNRs)
+tries = np.arange(0, total_tries, 1)
+index = 0
+for SNR in SNRs:
+    # print(SNR)
+    sucessos = 0
+    for trie in tries:
+        A = str.generate_ula_vectors(angles, 15, 1/2)
+        S = sig.gen_signal(2, 2000)
 
-u = str.generate_polarization_steering(reflections, 1)
+        u = str.generate_polarization_steering(reflections, 1)
 
-Z = str.merge_space_polarization_steering(A, u)
-X = np.matmul(Z, S)
-X = sig.add_noise(X, 30)
-DOAS, REFLECS = est.SAGE_Polarization(X, 2, 0.01, 1/2, 1)
+        Z = str.merge_space_polarization_steering(A, u)
+        X = np.matmul(Z, S)
+        X = sig.add_noise(X, SNR)
 
-possible_positions = []
+        DOAS, REFLECS = est.SAGE_Polarization(X, 2, 0.01, 1/2, 1)
+        DIST = est.distancePairing((angles, reflections), (DOAS, REFLECS), distances)
+        possible_positions = []
 
-for doa, reflection, distance in zip(DOAS, REFLECS, distances):
-    transmissor_longinquo_doa = geo.getPointsPositiveY(geo.getPointGivenSlopeDistance(geo.incidenceToSlope(doa, True), receiver, distance))[0]
-    transmissor_longinquo_reflection = geo.getPointsPositiveY(geo.getPointGivenSlopeDistance(geo.reflectionToSlope(doa, reflection, True), receiver, distance))[0]
-    possible_positions.append(geo.Line.fromPoints(transmissor_longinquo_doa, transmissor_longinquo_reflection))
+        for doa, reflection, distance in zip(DOAS, REFLECS, DIST):
 
-for position in possible_positions:
-    print(position.getY(0))
-    y = []
-    x = np.linspace(-50, 50)
-    for x_i in x:
-        y.append(position.getY(x_i))
-    plt.plot(x, y)
+            transmissor_longinquo_doa = geo.getPointsPositiveY(geo.getPointGivenSlopeDistance(geo.incidenceToSlope(doa, True), receiver, distance))[0]
+            transmissor_longinquo_reflection = geo.getPointsPositiveY(geo.getPointGivenSlopeDistance(geo.reflectionToSlope2(doa, reflection, True), receiver, distance))[0]
+            possible_positions.append(geo.Line.fromPoints(transmissor_longinquo_doa, transmissor_longinquo_reflection))
 
-plt.show()
+        estimated_pos = geo.getLinesIntercept(possible_positions[0], possible_positions[1])
+        error = geo.getPointDistance(transmitter, estimated_pos)
+        print(error)
+        distance_transmitter = geo.getPointDistance(estimated_pos, receiver)
 
-estimated_pos = geo.getLinesIntercept(possible_positions[0], possible_positions[1])
-error = geo.getPointDistance(transmitter, estimated_pos)
-print(estimated_pos)
-print(error)
+        if distance_transmitter > distance*1.1:
+            print('fail')
+        else:
+            errors[index] += error
+            sucessos += 1
+
+    errors[index] = errors[index]/sucessos
+    index += 1
